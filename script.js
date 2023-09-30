ef7c1a2eb746b146a8d88f06feaec42128415e3c
ReetamBG <r33t4m@gmail.com>
2023-09-30 13:34:39 +0530

    Added Saving Option of tasks (Final Commit)

--- script.js
+++ script.js
@@ -15,16 +15,32 @@ function addTask(){
         listContainer.appendChild(li);      //appends the li variable to the listContainer
         let span = document.createElement("span")
         span.innerHTML = ("\u00d7");
-        li.appendChild(span);       //To add a cross at the end of the task item      
+        li.appendChild(span);       //To add a cross at the end of the task item (u00d7 is the id used to describe the cross)     
     }
     inputBox.value = '';
+    saveData();
 }
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData();
     }
 }, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML); 
+    // Whatever innerHTML is in the listContainer will be stored into  a variable "data" in the browser
+}
+
+// Function to display the saved data as soon as the page is reloaded
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
+
